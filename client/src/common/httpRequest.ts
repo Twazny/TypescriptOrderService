ffe26291a447b95a7c4751c7f9182fb00528ca04
Tomasz Wazny <wazny.tomek@gmail.com>
2020-02-29 14:14:06 +0100

    simple service with web storage caching

--- client/src/common/httpRequest.ts
+++ client/src/common/httpRequest.ts
@@ -3,7 +3,6 @@ async function httpRequest<T> (req: RequestInfo): Promise<T> {
     const res = await fetch(req)
     if (!res.ok) {
         // zmienić na konkretną klasę błędu
-        res.status
         throw new Error(res.statusText);
     }
 
